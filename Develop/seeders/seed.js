aba0b96fb3d0bd31ab9b70893f64583eae310302
nnismithllc <65423204+nnismithllc@users.noreply.github.com>
2020-10-22 00:53:52 -0400

    downloaded dependencies and seed file

--- Develop/seeders/seed.js
+++ Develop/seeders/seed.js
@@ -3,145 +3,20 @@ let mongoose = require("mongoose");
 let db = require("../models");
 
 // Mongoose Connector to the Database
-mongoose.connect("mongodb://localhost/workittrack", {
+mongoose.connect("mongodb://localhost/workofftrack", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 // Seed Database Information
-let workoutSeed = [
-  {
-    day: new Date().setDate(new Date().getDate()-10),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Bicep Curl",
-        duration: 20,
-        weight: 100,
-        reps: 10,
-        sets: 4
-      }
-    ]
-  },
-  {
-    day: new Date().setDate(new Date().getDate()-9),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Lateral Pull",
-        duration: 20,
-        weight: 300,
-        reps: 10,
-        sets: 4
-      }
-    ]
-  },
-  {
-    day: new Date().setDate(new Date().getDate()-8),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Push Press",
-        duration: 25,
-        weight: 185,
-        reps: 8,
-        sets: 4
-      }
-    ]
-  },
-  {
-    day: new Date().setDate(new Date().getDate()-7),
-    exercises: [
-      {
-        type: "cardio",
-        name: "Running",
-        duration: 25,
-        distance: 4
-      }
-    ]
-  },
-  {
-    day: new Date().setDate(new Date().getDate()-6),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Bench Press",
-        duration: 20,
-        weight: 285,
-        reps: 10,
-        sets: 4
-      }
-    ]
-  },
-  {
-    day: new Date().setDate(new Date().getDate()-5),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Bench Press",
-        duration: 20,
-        weight: 300,
-        reps: 10,
-        sets: 4
-      }
-    ]
-  },
-  {
-    day:new Date().setDate(new Date().getDate()-4),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Quad Press",
-        duration: 30,
-        weight: 300,
-        reps: 10,
-        sets: 4
-      }
-    ]
-  },
-  {
-    day:new Date().setDate(new Date().getDate()-3),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Bench Press",
-        duration: 20,
-        weight: 300,
-        reps: 10,
-        sets: 4
-      }
-    ]
-  },
-  {
-    day:new Date().setDate(new Date().getDate()-2),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Military Press",
-        duration: 20,
-        weight: 300,
-        reps: 10,
-        sets: 4
-      }
-    ]
-  },
-  {
-  day: new Date().setDate(new Date().getDate()-1),
-  exercises: [
-    {
-      type: "resistance",
-      name: "Bench",
-      duration: 30,
-      distance: 2
-    }
-  ]
-}
+let workoffSeed = [
+
 
 ];
 
 // Seed Delete Function w/ Errors
-db.Workout.deleteMany({})
-  .then(() => db.Workout.collection.insertMany(workoutSeed))
+db.WorkOffTrack.enterMany({})
+  .then(() => db.WorkOffTrack.collection.insertMany(workoffSeed))
   .then(data => {
     console.log(data.result.n + " records inserted!");
     process.exit(0);
