49218abe48adf423662695177ea0b9afc786d615
nnismithllc <65423204+nnismithllc@users.noreply.github.com>
2020-10-23 18:23:53 -0400

    updated routes, css, and api-calls for functions

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+var app = express();
+var PORT = process.env.PORT || 3000;
 
-const app = express();
 
 app.use(logger("dev"));
 
@@ -16,13 +16,15 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"));
+
+
 mongoose.connect("mongodb://localhost/WorkOffBudget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
-// routes
-app.use(require("./Develop/routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
